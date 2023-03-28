9e6f2c32de4cef679756ef055dc46d8a205b8fbc
Abhijitsarker <abhijitsarker03@gmail.com>
2023-03-28 20:35:18 +0600

    player selector js

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+function selectPlayer(playerName) {
+  const name = document.getElementById(playerName).innerText;
+  const squad = document.getElementById("squad");
+  const list = document.createElement("li");
+  list.innerText = name;
+
+  squad.appendChild(list);
+}
+
+function selector(elementID, playerName) {
+  document.getElementById(elementID).addEventListener("click", function () {
+    selectPlayer(playerName);
+    this.disabled = true;
+  });
+}
+selector("btn-messi", "messi");
+selector("btn-neymar", "neymar");
+selector("btn-ronaldo", "ronaldo");
+selector("btn-mbappe", "mbappe");
+selector("btn-kane", "kane");
+selector("btn-modrich", "modrich");
+
+// document.getElementById("squad").addEventListener("click", function (event) {
+//   event.target.parentNode.removeChild(event.target);
+// });
